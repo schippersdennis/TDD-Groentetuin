e3f24b1608116f175441ae9a8126150206bb7526
Dennis Schippers <schippersdennis@gmail.com>
2020-12-27 12:45:54 +0100

    removed var

--- farm.js
+++ farm.js
@@ -2,20 +2,9 @@ const getYieldForPlant = (corn) => {
     return corn.yield;
 };
 
-const corn = {
-    name: 'corn',
-    yield: 3,
-};
-const input = {
-    crop: corn,
-    numCrops: 10,
-};
-
 const getYieldForCrop = (input) => {
     return input.crop.yield * input.numCrops;
 };
-
-getYieldForCrop(input);
 
 module.exports = {
     getYieldForPlant,
