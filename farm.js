bafbc3725a16d4f96dc7b17bda946886612813b8
Dennis Schippers <schippersdennis@gmail.com>
2020-12-27 15:25:04 +0100

    removed variables from farm.js

--- farm.js
+++ farm.js
@@ -6,18 +6,6 @@ const getYieldForCrop = (input) => {
     return input.crop.yield * input.numCrops;
 };
 
-const corn = {
-    name: 'corn',
-    yield: 3,
-};
-const pumpkin = {
-    name: 'pumpkin',
-    yield: 4,
-};
-const crops = [
-    { crop: corn, numCrops: 5 },
-    { crop: pumpkin, numCrops: 2 },
-];
 const getTotalYield = (crops) => {
     let total = 0;
     crops.crops.forEach((item) => {
